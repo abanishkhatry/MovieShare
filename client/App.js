348d3f629dfa0edadc53a3c33df096ae234d8184
abanishkhatry <khatriavanish@gmail.com>
2025-06-10 12:52:37 -0500

    Setting up App Foundation

---
+++ client/App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to MovieShare 🎬</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
