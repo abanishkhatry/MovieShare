112713d97744d46e6fdb4afbfc4ced9cb342ed6c
abanishkhatry <khatriavanish@gmail.com>
2025-06-11 12:36:39 -0500

    Modified App.js by including constant styles

--- client/App.js
+++ client/App.js
@@ -1,20 +1,24 @@
-// App.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react'; // React library for component-based UI
+import { View, Text, StyleSheet } from 'react-native'; // Native components
+import { COLORS, FONT_SIZES } from './styles/global'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to MovieShare 🎬</Text>
+    <View style={styles.container}>  {/* View is like a <div> */}
+      <Text style= {styles.welcome}> Welcome to MovieShare 🎬</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ // Styling like CSS-in-JS
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1, // Takes full screen
+    justifyContent: 'center', // Centers vertically
+    alignItems: 'center', // Centers horizontally
+  },
+  welcome: {
+    fontSize: FONT_SIZES.lg,
+    color: COLORS.primary,
   },
 });
 
