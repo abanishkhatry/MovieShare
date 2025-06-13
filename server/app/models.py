44fdcaf68abb2a3c186895b7d8c646dccdd63795
abanishkhatry <khatriavanish@gmail.com>
2025-06-13 12:41:10 -0500

    Switched to storing PostgreSQL table structures using SQLAlchemy

--- server/app/models.py
+++ server/app/models.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel, EmailStr
-from datetime import datetime
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.sql import func
+from .database import Base
 
-# seeking informations that are only required for login/authentication
-class UserAuth(BaseModel): 
-    username: str
-    email: EmailStr
-    hashed_password: str 
-    created_at: datetime 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
