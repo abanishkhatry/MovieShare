84f1d10e06899ef3a413d97f15da4a9e9f0a6849
abanishkhatry <khatriavanish@gmail.com>
2025-06-15 12:46:57 -0500

    Added schemas for Post

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 # while registering a new user
 class UserCreate(BaseModel):
@@ -10,3 +11,22 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str     
+
+# information user have to fill for posting
+class PostBase(BaseModel):
+    title: str
+    content: str    
+
+# inherits from PostBase and helps to create a post
+class PostCreate(PostBase):
+    pass    
+
+# this is used in returning a receipt to the frontend/client that the server saved/created the post
+class PostOut(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+
+    class Config:
+        #  allows Pydantic to work directly with SQLAlchemy objects
+        orm_mode = True
