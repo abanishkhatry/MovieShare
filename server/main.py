8bad37e15889f0de3ace78f71c1901649408ee43
abanishkhatry <khatriavanish@gmail.com>
2025-06-15 12:48:45 -0500

    Included post route

--- server/main.py
+++ server/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
-from app.routes import router
+from app.routes import routes
 from app.database import engine
 from app import models
+from app.routes import post
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(router)
+# Include the existing router (probably for /signup, /login etc.)
+app.include_router(routes.router)
+
+# Include the post router
+app.include_router(post.router)
 
 @app.get("/")
 def read_root():
